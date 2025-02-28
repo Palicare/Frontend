59900d5f1e73a0f6bda7eb4788305d27f820647b
unknown <seeliger.andreas@web.de>
2025-02-28 17:00:56 +0100

    added audio functionnality and started to work on fetching patient data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Login from "./pages/LoginPage";
+import Patientenprofile from "./pages/Patientenprofile";
+import Assistant from "./pages/Assistant"
 
 export default function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Login />} />
+                <Route path="/patientenprofile" element={<Patientenprofile/>}/>
+                <Route path="/assistant/:patientId" element={<Assistant/>}/>
             </Routes>
         </Router>
     )
---
+++ src/pages/PatientenProfil.jsx
@@ -0,0 +1,5 @@
+export default function() {
+    return <>
+        <h1>Patienten Profil</h1>
+    </>
+}
